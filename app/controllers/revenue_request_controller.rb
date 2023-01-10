ea7540a570f1604afec7e12e325c936330a95f61
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-01-09 19:02:31 -0500

    Deletes revenues request controller

--- app/controllers/revenue_request_controller.rb
+++
@@ -1,8 +0,0 @@
-class RevenueRequestController < ApplicationController
-  def request
-    request = HTTP.get("https://data.miamigov.com/resource/ub3m-qgg5.json")
-
-    new_request = RevenueRequest.new(request)
-    new_request.to_database
-  end
-end
