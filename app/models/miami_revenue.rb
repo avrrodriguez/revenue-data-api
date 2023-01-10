f60dc4f83c84176e433d177c0878f686743b1b57
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-01-09 19:01:51 -0500

    Gets all data points from API and changes where request is made to the service

--- app/models/miami_revenue.rb
+++ app/models/miami_revenue.rb
@@ -1,3 +1,5 @@
 class MiamiRevenue < ApplicationRecord
+  DEPARTMENT_DESCRIPTIONS = { BUILDING: "Building" }.freeze
   scope :no_budget, -> { where("adopted_budget = 0") }
+  scope :building_department, -> { where("department_description = ?", DEPARTMENT_DESCRIPTIONS[:BUILDING]) }
 end
--- app/services/revenue_request.rb
+++ app/services/revenue_request.rb
@@ -1,17 +1,50 @@
 class RevenueRequest
-  def initialize(request)
-    @revenue = request.parse(:json)
+  def call
+    offset = 0
+    while true
+      request = HTTP.get("https://data.miamigov.com/resource/ub3m-qgg5.json?$order=:id&$offset=#{offset}")
+      response = request.parse(:json)
+
+      to_database(response)
+
+      break if response.length < 1000
+
+      offset += 1000
+    end
   end
 
-  def print
-    puts @revenue.length
-  end
+  def to_database(response)
+    response.each do |revenue_item|
+      miami_revenue = MiamiRevenue.new(revenue_type: revenue_item["type"],
+                                       fy: revenue_item["fy"],
+                                       fund_type: revenue_item["fund_type"],
+                                       fund_number: revenue_item["fund_number"],
+                                       fund_description: revenue_item["fund_description"],
+                                       department_function_description: revenue_item["department_function"],
+                                       department_description: revenue_item["department_description"],
+                                       organization_number: revenue_item["organization"],
+                                       organization_description: revenue_item["organization_description"],
+                                       object_type: revenue_item["object"],
+                                       object_number: revenue_item["object_category_description"],
+                                       object_description: revenue_item["object_description"],
+                                       oct: revenue_item["oct"],
+                                       nov: revenue_item["nov"],
+                                       dec: revenue_item["dec"],
+                                       jan: revenue_item["jan"],
+                                       feb: revenue_item["feb"],
+                                       mar: revenue_item["mar"],
+                                       apr: revenue_item["apr"],
+                                       may: revenue_item["may"],
+                                       jun: revenue_item["jun"],
+                                       jul: revenue_item["jul"],
+                                       aug: revenue_item["aug"],
+                                       sep: revenue_item["sep"],
+                                       thirteenth_month: revenue_item["_13th_month"],
+                                       total: revenue_item["total"],
+                                       adopted_budget: revenue_item["adopted_budget"],
+                                       amended_budget: revenue_item["amended_budget"])
 
-  def to_database
-    @revenue.each do |revenue|
-      miami_revenue = MiamiRevenue.new(revenue_type: revenue["type"], fy: revenue["fy"], fund_type: revenue["fund_type"], fund_number: revenue["fund_number"], fund_description: revenue["fund_description"], department_function_description: revenue["department_function"], department_description: revenue["department_description"], organization_number: revenue["organization"], organization_description: revenue["organization_description"], object_type: revenue["object"], object_number: revenue["object_category_description"], object_description: revenue["object_description"], oct: revenue["oct"], nov: revenue["nov"], dec: revenue["dec"], jan: revenue["jan"], feb: revenue["feb"], mar: revenue["mar"], apr: revenue["apr"], may: revenue["may"], jun: revenue["jun"], jul: revenue["jul"], aug: revenue["aug"], sep: revenue["sep"], thirteenth_month: revenue["_13th_month"], total: revenue["total"], adopted_budget: revenue["adopted_budget"], amended_budget: revenue["amended_budget"])
-
-      miami_revenue.save
+      miami_revenue.save!
     end
   end
 end
