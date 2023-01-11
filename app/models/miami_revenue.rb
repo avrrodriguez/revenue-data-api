35daf38625af9702b450f688f469dde49402b9a6
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-01-11 12:53:45 -0500

    Adds yearly department earnings scope, Adds comments in schema explaining 13th month and encumbrance

--- app/models/miami_revenue.rb
+++ app/models/miami_revenue.rb
@@ -2,4 +2,5 @@ class MiamiRevenue < ApplicationRecord
   DEPARTMENT_DESCRIPTIONS = { BUILDING: "Building" }.freeze
   scope :no_budget, -> { where("adopted_budget = 0") }
   scope :building_department, -> { where("department_description = ?", DEPARTMENT_DESCRIPTIONS[:BUILDING]) }
+  scope :yearly_department_earnings, -> { select("department_description, fy, sum(total) as total_revenue").group("department_description, fy") }
 end
