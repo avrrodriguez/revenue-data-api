1da0fd020eb7a2c6db82cb562f9af037e2fff50d
Abraham V Rodriguez <pqavrr835@gmail.com>
2023-03-08 14:38:46 -0500

    Adds rank columns method, initialize and dfill column rank arrayas method, and determine rank distance method in spearman rank correlation request service

--- app/controllers/revenues_controller.rb
+++ app/controllers/revenues_controller.rb
@@ -7,7 +7,7 @@ class RevenuesController < ApplicationController
 
   def spearman_rank_correlation
     spearman_correlation_coefficient = SpearmanRankCorrelationRequest.new("thirteenth_month", "adopted_budget")
-    p spearman_correlation_coefficient.column_name1[0..4]
+    p spearman_correlation_coefficient.column1_data[0..4]
 
     render json: { "message": "connected" }
   end
--- app/services/spearman_rank_correlation_request.rb
+++ app/services/spearman_rank_correlation_request.rb
@@ -1,17 +1,66 @@
 class SpearmanRankCorrelationRequest
   attr_reader :column1_data, :column2_data
 
-  def initialize(column_name1, column_name2)
-    @column1_data = get_data_from_column_as_array(column_name1)
-    @column2_data = get_data_from_column_as_array(column_name2)
+  def initialize(column1_name, column2_name)
+    @column1_data = get_data_from_column_as_array(column1_name)
+    @column2_data = get_data_from_column_as_array(column2_name)
+
+    initialize_and_fill_column_rank_arrays()
   end
 
   def get_data_from_column_as_array(column_name)
     MiamiRevenue.pluck(column_name.to_sym)
   end
 
-  def sort_array_desc_order(column_name)
-    column_name.sort().reverse
+  def sort_array_desc_order(column_data)
+    column_data.sort().reverse
+  end
+
+  def initialize_and_fill_column_rank_arrays
+    @column1_data_ranks = Array.new(@column1_data.length)
+    @column2_data_ranks = Array.new(@column2_data.length)
+
+    rank_columns()
+
+    p "ranks", @column1_data_ranks[0..4]
+    p @column2_data_ranks[0..4]
+  end
+
+  def rank_columns
+    sort_array_desc_order(@column1_data).each_with_index do |number, index|
+      @column1_data_ranks[@column1_data.index(number)] = index + 1
+    end
+
+    sort_array_desc_order(@column2_data).each_with_index do |number, index|
+      @column2_data_ranks[@column2_data.index(number)] = index + 1
+    end
+
+    @column1_data_ranks = @column1_data_ranks.map do |number|
+      if number == nil
+        number = 0
+      else
+        number = number
+      end
+    end
+
+    @column2_data_ranks = @column2_data_ranks.map do |number|
+      if number == nil
+        number = 0
+      else
+        number = number
+      end
+    end
+
+    p "data 1 rank", @column1_data_ranks[0..4]
+    p "data 2 rank", @column2_data_ranks[0..4]
+  end
+
+  def determine_rank_distance
+    @rank_distance = []
+
+    for num in 0..(@column1_data_ranks.length - 1)
+      @rank_distance << (@column1_data_ranks[num] - @column2_data_ranks[num]).abs
+    end
   end
 
   def spearman_rank_correlation
